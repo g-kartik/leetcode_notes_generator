805469bb277404a8e4e7fd8ae011d5687065f331
G Ajeet <55391283+gajeet11000@users.noreply.github.com>
2026-08-23 13:01:11 +0530

    refactored parsers

---
+++ modules/leetcode/parsers/__init__.py
@@ -0,0 +1,7 @@
+from .question_content.html_to_markdown import html_to_markdown
+from .question_content.html_to_plain_text import html_to_plain_text
+from .api_response_parsers import (
+    gql_question_data,
+    gql_submission_list,
+    gql_submission_data,
+)
---
+++ modules/leetcode/parsers/api_response_parsers.py
@@ -0,0 +1,61 @@
+from datetime import UTC, datetime
+
+from modules.leetcode.settings import leetcode_settings
+
+
+def gql_question_data(response_data: dict) -> dict:
+    # Contains raw leetcode question in html format
+    question_data = response_data.get("data", {}).get("question", {})
+
+    raw_html_question = question_data.get("content", "")
+
+    # Contains tag contains normal tag and slugified version (name, slug) vars.
+    topic_tags = question_data.get("topicTags", [])
+
+    slug = question_data.get("titleSlug")
+    title = question_data.get("title")
+    id = question_data.get("questionFrontendId")
+    url = f"{leetcode_settings.BASE_URL}/problems/{slug}/"
+    difficulty = question_data.get("difficulty")
+    category = question_data.get("categoryTitle")
+
+    return {
+        "id": id,
+        "url": url,
+        "slug": slug,
+        "title": title,
+        "tags": topic_tags,
+        "category": category,
+        "difficulty": difficulty,
+        "content_html": raw_html_question,
+    }
+
+
+def gql_submission_list(response_data: dict) -> list[dict]:
+    return (
+        response_data.get("data", {})
+        .get("questionSubmissionList", {})
+        .get("submissions", [])
+    )
+
+
+def gql_submission_data(response_data: dict) -> dict:
+    data = response_data.get("data", {}).get("submissionDetails", {})
+
+    if not data:
+        return {}
+
+    lang = data.get("lang", {}).get("name", None)
+    code = data.get("code", None)
+    timestamp = data.get("timestamp", None)
+
+    if timestamp:
+        submission_date = datetime.fromtimestamp(timestamp, tz=UTC).isoformat()
+    else:
+        submission_date = None
+
+    return {
+        "lang": lang,
+        "code": code,
+        "submission_date": submission_date,
+    }
